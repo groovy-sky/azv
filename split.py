99dc0c1d79edf866011406f9c4224eae182b9602
groovy-sky <groovy-sky>
2023-11-07 22:29:07 +0200

    fixed and now works

--- split.py
+++ split.py
@@ -13,18 +13,35 @@ def validate_mask(new_prefix, old_prefix):
         return False
     return True
 
-def split_cidr(ip_network, new_prefix):
+def split_cidr(ip_network, new_prefix, verbose=False):
     if not validate_cidr(ip_network):
-        print("Error: Invalid CIDR notation.")
+        if verbose:
+            print("Error: Invalid CIDR notation.")
         return None
     try:
         ip_network_obj = ipaddress.ip_network(ip_network)
         old_prefix = ip_network_obj.prefixlen
         if not validate_mask(new_prefix, old_prefix):
-            print("Error: Invalid subnet mask.")
+            if verbose:
+                print("Error: Invalid subnet mask.")
             return None
         subnets = list(ip_network_obj.subnets(new_prefix=new_prefix))
         return [str(subnet) for subnet in subnets]
     except ValueError as e:
-        print(f"Error: {e}")
+        if verbose:
+            print(f"Error: {e}")
         return None
+
+def main():
+    parser = argparse.ArgumentParser(description='Split a CIDR block into smaller subnets.')
+    parser.add_argument('ip_network', type=str, help='The IP network in CIDR format.')
+    parser.add_argument('new_prefix', type=int, help='The new prefix length.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output.')
+    args = parser.parse_args()
+
+    result = split_cidr(args.ip_network, args.new_prefix, verbose=args.verbose)
+    if result is not None:
+        print(result)
+
+if __name__ == "__main__":
+    main()
